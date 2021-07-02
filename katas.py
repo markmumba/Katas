76a29b805959d3549cbc65f05a17f5c982b94e8d
markmumba <markmumba01@gmail.com>
2021-07-02 10:07:15 +0300

    Added two katas to it

---
+++ katas.py
@@ -0,0 +1,25 @@
+# .................................................................
+# code to reverse a list given letter to dictate which side to do so 
+# .................................................................
+# def flip(d, a):
+#     if d == "R":
+#        a.sort()
+#        return a
+#     else:
+#         a.sort(reverse=True)
+#         return a
+   
+# words= flip('L',[2,4,3,5,6,7,8,6,1])
+# print(words)
+
+
+# ...................................................................
+# code to return the value sitting in the even idexes inside the list
+# ...................................................................
+# def odd_place(my_list):
+#     empty = []
+#     for i in range(len(my_list)):
+#         if i % 2 ==0:
+#             empty.append(my_list[i])
+#     print(empty)          
+# my_list=odd_place([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])            
---
+++ moringa.py
@@ -0,0 +1,44 @@
+# import datetime
+# now = datetime.datetime.now().year
+# Age= int(input("Enter your Age boy"))
+# Birth=now-Age
+# Later=Birth + 80
+
+# print("Your Birth date is \n",Birth)
+# print("You will turn 80 in\n", Later)
+
+
+
+
+
+# def not_taken(arr):
+#     for i in range(1, 21):
+#         if  i in arr:
+#             continue
+#         print (i)
+
+# test=not_taken([6, 1, 7, 8, 3, 12])
+# print (test)
+
+# my_dic={1:"Joe",2:"Kim",3:"Pulev",4:"jane",5:"yvone",6:"jim"}
+# print(my_dic.copy())
+
+# Dictionary Comprehension
+# squares = {x: x*x for x in range(6)}
+# print(squares[3])
+# print(squares)
+#item price in dollars
+# old_price = {'milk': 1.02, 'coffee': 2.5, 'bread': 2.5}
+
+# dollar_to_pound = 0.76
+# # new_price = {item: value*dollar_to_pound for (item, value) in old_price.items()}
+# # print(new_price)
+
+# for (item, value) in old_price.items():
+#     old_price[item]=value*dollar_to_pound
+# print(old_price)
+
+odd =[9]
+odd.insert(1,10)
+print(odd)
+
